faff4bd157e49ab22bc1f7ade1025b616711cc16
SantiagoBetancurCardona97 <santibetancur97@gmail.com>
2021-02-28 16:52:26 -0500

    Add task

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
- import React from 'react'
+import React, {useState} from 'react'
+import { initial, isEmpty } from 'lodash'
+import shortid from 'shortid'
+
  function App (){
+  const [pet, setPet] = useState("")
+  const [pets, setPets] = useState([])
+
+  const addPet = (e) =>{
+    e.preventDefault()
+    if(isEmpty(pet)){
+      console.log("pet empty")
+      return
+    }
+ 
+    const newPet = {
+      id: shortid.generate(),
+      name: pet
+    }
+    setPets([...pets, newPet])
+    setPet("")
+
+  }
+  
+
    return (
     <div>
       <div className="jumbotron text-center">
         <h2>Mascotas</h2>
         <hr/>
         <div className="container">
-          <form>
+          <form onSubmit={addPet}>
           <div className="row">
-            <div className="col-sm-2">
+            <div className="col-2">
             <h10>Nombre de Mascota</h10>
-            <input type="text" placeholder="Escribir Nombre"></input>
+            <input type="text" placeholder="Escribir Nombre"
+             onChange= {(text) => setPet(text.target.value)}
+             value={pet}></input>
              <div>
               <h10>Tipo de Mascota</h10>
               <input type="text" placeholder="Perro, gato u otro"></input>
              </div>
             </div>
-            <div className="col-sm-2">
+            <div className="col-2">
               <h10>Raza de  Mascota</h10>
               <input type="text" placeholder="Escribir raza"></input>
               <div>
@@ -24,7 +49,7 @@
                <input type="text" placeholder="DD-MM-AAAA"></input>  
               </div>
             </div>
-            <div className="col-sm-2">
+            <div className="col-2">
               <h10>Propietario</h10>
               <input type="text" placeholder="Escribir nombre"></input>
                <div>
@@ -32,11 +57,11 @@
                 <input type="text" placeholder="Escribir E-mail"></input>
                </div>
             </div>
-            <div className="col-sm-2">
+            <div className="col-2">
               <h10>Direccion</h10>
               <input type="text" placeholder="Escribir Direccion"></input>
             </div>
-            <div className="col-sm-2">
+            <div className="col-2">
               <button className="btn btn float-right mx-2">Agregar</button>
               <button className="btn btn-dark float-right">Modificar</button>
             </div>        
